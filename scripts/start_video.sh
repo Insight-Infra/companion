f50192ba212a595a432cd4b8a0c53c8f99058da0
Eliot-Insight <eliot@insightinfra.com.au>
2020-09-14 09:52:20 +1000

    extra top camera

--- scripts/start_video.sh
+++ scripts/start_video.sh
@@ -6,11 +6,13 @@ if [ -z "$1" ]; then
     HEIGHT=$(cat ~/vidformat.param | xargs | cut -f2 -d" ")
     FRAMERATE=$(cat ~/vidformat.param | xargs | cut -f3 -d" ")
     DEVICE=$(cat ~/vidformat.param | xargs | cut -f4 -d" ")
+    DEVICE2=$(cat ~/vidformat.param | xargs | cut -f5 -d" ")
 else
     WIDTH=$1
     HEIGHT=$2
     FRAMERATE=$3
     DEVICE=$4
+    DEVICE2=$5
 fi
 
 echo "start video with width $WIDTH height $HEIGHT framerate $FRAMERATE device $DEVICE"
@@ -39,11 +41,12 @@ if [ $? != 0 ]; then
 fi
 
 # load gstreamer options
-gstOptions=$(tr '\n' ' ' < $HOME/gstreamer2.param)
-gstOptionsAux=$(tr '\n' ' ' < $HOME/gstreamer2_aux.param)
+gstOptionsBR=$(tr '\n' ' ' < $HOME/gstreamer2_BR.param)
+gstOptionsBR2=$(tr '\n' ' ' < $HOME/gstreamer2_BR2.param)
+gstOptionsAHD=$(tr '\n' ' ' < $HOME/gstreamer2_AHD.param)
 
 # start auxiliary camera in a separate thread
-screen -dm -S video_aux bash -c "export LD_LIBRARY_PATH=/usr/local/lib && gst-launch-1.0 -v v4l2src do-timestamp=true $gstOptionsAux" &
+screen -dm -S video_ahd bash -c "export LD_LIBRARY_PATH=/usr/local/lib && gst-launch-1.0 -v v4l2src do-timestamp=true $gstOptionsAHD" &
 
 # make sure framesize and framerate are supported
 
@@ -55,10 +58,12 @@ gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true num-buffers=1 ! video
 
 if [ $? != 0 ]; then
     echo "Device is not capable of specified format, using device current settings instead"
-    bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264 $gstOptions"
+    screen -dm -S video_br bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264 $gstOptionsBR" &
+    screen -dm -S video_br2 bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264 $gstOptionsBR2"
 else
-    echo "starting device $DEVICE with width $WIDTH height $HEIGHT framerate $FRAMERATE options $gstOptions"
-    bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptions"
+    echo "starting devices ($DEVICE, $DEVICE2) with width $WIDTH height $HEIGHT framerate $FRAMERATE options ($gstOptionsBR, $gstOptionsBR2)"
+    screen -dm -S video_br bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptionsBR" &
+    screen -dm -S video_br2 bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptionsBR2"
     # if we make it this far, it means the gst pipeline failed, so load the backup settings
     cp ~/vidformat.param.bak ~/vidformat.param && rm ~/vidformat.param.bak
 fi
