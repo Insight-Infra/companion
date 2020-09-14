1b9d3e0d5feaa59ce018f787e1eaabff60872e0b
Eliot-Insight <eliot@insightinfra.com.au>
2020-09-14 11:48:38 +1000

    all streams online

--- scripts/start_video.sh
+++ scripts/start_video.sh
@@ -15,7 +15,7 @@ else
     DEVICE2=$5
 fi
 
-echo "start video with width $WIDTH height $HEIGHT framerate $FRAMERATE device $DEVICE"
+echo "start videos with width $WIDTH height $HEIGHT framerate $FRAMERATE devices $DEVICE, $DEVICE2"
 
 # Load Pi camera v4l2 driver
 if ! lsmod | grep -q bcm2835_v4l2; then
@@ -42,7 +42,7 @@ fi
 
 # load gstreamer options
 gstOptionsBR=$(tr '\n' ' ' < $HOME/gstreamer2_BR.param)
-gstOptionsBR2=$(tr '\n' ' ' < $HOME/gstreamer2_BR2.param)
+gstOptionsBRT=$(tr '\n' ' ' < $HOME/gstreamer2_BRT.param)
 gstOptionsAHD=$(tr '\n' ' ' < $HOME/gstreamer2_AHD.param)
 
 # start auxiliary camera in a separate thread
@@ -50,8 +50,9 @@ screen -dm -S video_ahd bash -c "export LD_LIBRARY_PATH=/usr/local/lib && gst-la
 
 # make sure framesize and framerate are supported
 
-# workaround to make sure we don't attempt 1080p@90fps on pi camera
+# workaround to make sure we don't attempt 1080p@90fps on pi cameras
 v4l2-ctl --device $DEVICE --set-parm $FRAMERATE
+v4l2-ctl --device $DEVICE2 --set-parm $FRAMERATE
 
 echo "attempting device $DEVICE with width $WIDTH height $HEIGHT framerate $FRAMERATE options $gstOptions"
 gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true num-buffers=1 ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 ! h264parse ! queue ! rtph264pay config-interval=10 pt=96 ! fakesink
@@ -59,11 +60,11 @@ gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true num-buffers=1 ! video
 if [ $? != 0 ]; then
     echo "Device is not capable of specified format, using device current settings instead"
     screen -dm -S video_br bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264 $gstOptionsBR" &
-    screen -dm -S video_br2 bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264 $gstOptionsBR2"
+    screen -dm -S video_brt bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264 $gstOptionsBRT"
 else
-    echo "starting devices ($DEVICE, $DEVICE2) with width $WIDTH height $HEIGHT framerate $FRAMERATE options ($gstOptionsBR, $gstOptionsBR2)"
+    echo "starting devices ($DEVICE, $DEVICE2) with width $WIDTH height $HEIGHT framerate $FRAMERATE options ($gstOptionsBR, $gstOptionsBRT)"
     screen -dm -S video_br bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptionsBR" &
-    screen -dm -S video_br2 bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptionsBR2"
+    screen -dm -S video_brt bash -c "export LD_LIBRARY_PATH=/usr/local/lib/ && gst-launch-1.0 -v v4l2src device=$DEVICE2 do-timestamp=true ! video/x-h264, width=$WIDTH, height=$HEIGHT, framerate=$FRAMERATE/1 $gstOptionsBRT"
     # if we make it this far, it means the gst pipeline failed, so load the backup settings
     cp ~/vidformat.param.bak ~/vidformat.param && rm ~/vidformat.param.bak
 fi
