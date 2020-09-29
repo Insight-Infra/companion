4b3f718d3aa66917ea5151d19bced6e1ee02bc21
Eliot-Insight <eliot@insightinfra.com.au>
2020-09-29 12:05:53 +1000

    restart ut-read with mavproxy

--- scripts/restart-mavproxy.sh
+++ scripts/restart-mavproxy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+screen -X -S thickness quit
 screen -X -S mavproxy quit
-sudo -H -u pi screen -dm -S mavproxy \
-    $COMPANION_DIR/tools/telem.py
+
+sudo -H -u pi screen -dm -S thickness $COMPANION_DIR/tools/ut_read.py &
+sudo -H -u pi screen -dm -S mavproxy $COMPANION_DIR/tools/telem.py
