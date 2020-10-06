39215269df1458a8d85d956a803ad43e94113847
Eliot-Insight <65874598+Eliot-Insight@users.noreply.github.com>
2020-10-06 15:33:21 +1100

    two cameras

--- scripts/restart_video.sh
+++ scripts/restart_video.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 if screen -ls | grep video; then
+        screen -X -S video_brf quit
+        screen -X -S video_brt quit
         screen -X -S video quit
 fi
 
 # this is followed up in start_video.sh
 cp /home/pi/vidformat.param /home/pi/vidformat.param.bak
 
-if [ "$#" == 4 ]; then
+if [ "$#" == 5 ]; then
         echo $1 > /home/pi/vidformat.param
         echo $2 >> /home/pi/vidformat.param
         echo $3 >> /home/pi/vidformat.param
         echo $4 >> /home/pi/vidformat.param
+        echo $5 >> /home/pi/vidformat.param
 else
         echo "Invalid number of parameters [$#]: $@"
 fi
