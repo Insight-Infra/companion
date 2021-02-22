71e8ddcbe4d8a43e74d5c5cf69564c39dcb7d252
Eliot-Insight <eliot@insightinfra.com.au>
2021-02-22 14:27:14 +1100

    RPi4 ping360 + 2*video

--- tools/streamer.py
+++ tools/streamer.py
@@ -3,18 +3,18 @@
 import os
 from time import sleep
 import sys
-home = os.environ['HOME']
+script = os.environ['HOME'] + "/companion/scripts/start_video.sh"
 
 os.system("sudo modprobe bcm2835-v4l2")
 
 while True:
-  while (os.system("ls /dev/video* 2>/dev/null") != 0) or (os.path.isfile(home + "/companion/start_video.sh")):
-    sleep(5)
-  if len(sys.argv) == 1:
-    os.system(home + "/companion/scripts/start_video.sh $(cat /home/pi/vidformat.param)")
-  else:
-    os.system(home + "/companion/scripts/start_video.sh " + " ".join(sys.argv[1:]))
+  while (os.system("ls /dev/video* 2>/dev/null") != 0) or not os.path.isfile(script):
+    sleep(5) # wait for video to exist
 
-  # wait until one of the cameras fails
-  while not any(os.system('screen -ls | grep video_br{}'.format(video)) for video in 'ft'):
-    sleep(2)
+  if len(sys.argv) == 1:
+    os.system(script + " $(cat /home/pi/vidformat.param)")
+  else:
+    os.system(script + " ".join(sys.argv[1:]))
+  sleep(2)
+
+
